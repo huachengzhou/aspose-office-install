c86c932835e7244cdd93d27dac9108a898148ecc
zhouchenhua <noatnu@163.com>
2019-01-16 23:20:39 +0800

    睡前提交

---
+++ src/test/java/org/test/Demo3.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import com.help.TestFile;
+import org.testng.annotations.Test;
+
+/**
+ * @Author noatn
+ * @Description
+ * @createDate 2019/1/16
+ **/
+public class Demo3 {
+
+    /**
+     * 书签删除
+     * @throws Exception
+     */
+    @Test
+    public void test()throws Exception{
+        final String dataPath = TestFile.getTestDataParentDir(this.getClass());
+        //https://bbs.csdn.net/topics/310190259
+    }
+
+}
