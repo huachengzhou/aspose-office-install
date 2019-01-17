0ad2f0552bd2dd407acf9e401ed1a9d8b9c325d6
zhouchenhua <noatnu@163.com>
2019-01-17 09:48:58 +0800

    书签替换与删除

--- src/test/java/org/test/Demo3.java
+++ src/test/java/org/test/Demo3.java
@@ -3,11 +3,14 @@ package org.test;
 import com.aspose.words.Bookmark;
 import com.aspose.words.Document;
 import com.aspose.words.DocumentBuilder;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.help.TestFile;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -33,7 +36,7 @@ public class Demo3 {
 
         Map<String, String> map = Maps.newHashMap();
         map.put("文号", "测试1号!");
-        replaceBookmark(String.format("%s%s", dataPath, "房产预评报告模板.doc"), map);
+        replaceBookmark(String.format("%s%s", dataPath, "房产预评报告模板.doc"), map,true);
     }
 
     private void replaceBookmark(String filePath, Map<String, String> map) throws Exception {
@@ -57,6 +60,41 @@ public class Demo3 {
 //            doc.getRange().getBookmarks().remove(bookmark);
         }
         doc.save(String.format("%s%s",dataPath, UUID.randomUUID().toString()+".doc"));
+    }
+
+    /**
+     * Map<String, String> map == > 书签名称,需要替换的内容
+     * @param filePath 具体文件路径
+     * @param map 书签名称,需要替换的内容
+     * @param deleteBookMark 是否需要在替换完成时删除书签
+     * @throws Exception
+     */
+    private   void replaceBookmark(String filePath, Map<String, String> map, boolean deleteBookMark) throws Exception {
+        List<String> bookmarkList = Lists.newArrayList();
+        if (StringUtils.isBlank(filePath)) {
+            throw new Exception("error: empty file path");
+        }
+        if (map == null || map.isEmpty()) {
+            throw new Exception("error: empty map");
+        }
+        Document doc = new Document(filePath);
+        for (Map.Entry<String, String> stringStringEntry : map.entrySet()) {
+            Bookmark bookmark = doc.getRange().getBookmarks().get(stringStringEntry.getKey());
+            bookmark.setText(stringStringEntry.getValue());
+            bookmarkList.add(bookmark.getName());
+        }
+        String dataPathA = String.format("%s%s",dataPath, UUID.randomUUID().toString()+".doc") ;
+        doc.save(dataPathA);
+        if (deleteBookMark) {
+            Document docDelete = new Document(dataPathA);
+            if (CollectionUtils.isNotEmpty(bookmarkList)) {
+                for (String bookmark : bookmarkList) {
+                    //删除书签
+                    docDelete.getRange().getBookmarks().remove(bookmark);
+                }
+            }
+            docDelete.save(dataPathA);
+        }
     }
 
 }
